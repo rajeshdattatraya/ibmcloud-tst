a5b329188e765b83227aac522152f12732377ea7
ANIL GEORGE <anilgeorge@in.ibm.com>
2020-06-19 16:46:42 +0530

    US60 front-end SME Candidate Search commit 19Jun2020

--- src/app/components/technical-interview-list/technical-interview-list.component.ts
+++ src/app/components/technical-interview-list/technical-interview-list.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, NgZone } from '@angular/core';
+import { Component, OnInit, NgZone,Output, EventEmitter } from '@angular/core';
 import { ApiService } from '../../service/api.service';
-import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { FormGroup, FormBuilder, Validators,FormControl } from "@angular/forms";
 import { ActivatedRoute, Router } from '@angular/router';
 import { browserRefresh } from '../../app.component';
 
@@ -18,9 +18,12 @@ export class TechnicalInterviewListComponent implements OnInit {
   config: any;
   emailSelected = "";
   quizNumber;
+  technicalInterviewCandidateList: any = [];
+  filteredUsers: any[] = [];
+  form: FormGroup;
+  @Output() groupFilters: EventEmitter<any> = new EventEmitter<any>();
 
-
-  constructor(private route: ActivatedRoute, private router: Router, private apiService: ApiService, private ngZone: NgZone) {
+  constructor(private route: ActivatedRoute, private router: Router, private apiService: ApiService, private ngZone: NgZone,private fb: FormBuilder) {
     this.config = {
       currentPage: 1,
       itemsPerPage: 5,
@@ -31,6 +34,10 @@ export class TechnicalInterviewListComponent implements OnInit {
       this.userName = this.router.getCurrentNavigation().extras.state.username;
       this.accessLevel = this.router.getCurrentNavigation().extras.state.accessLevel;
     }
+    this.form = this.fb.group({
+      employeeName: new FormControl(''),
+      JRSS: new FormControl('')
+      });
     route.queryParams.subscribe(
       params => this.config.currentPage = params['page'] ? params['page'] : 1);
     this.getTechnicalInterviewList();
@@ -60,7 +67,7 @@ export class TechnicalInterviewListComponent implements OnInit {
     if (this.emailSelected == "") {
       alert("please select the candidate")
     }
-    else {      
+    else {
       this.router.navigate(['/technical-list/', this.emailSelected], { state: { username: this.userName, accessLevel: this.accessLevel } })
     }
   }
@@ -74,6 +81,44 @@ export class TechnicalInterviewListComponent implements OnInit {
   getTechnicalInterviewList() {
     this.apiService.getTechnicalInterviewList().subscribe((data) => {
       this.TechnicalInterviewList = data;
+      this.technicalInterviewCandidateList=data;
     })
   }
+
+  search(filters: any): void {
+    Object.keys(filters).forEach(key => filters[key] === '' ? delete filters[key] : key);
+    this.filterUserList(filters,this.technicalInterviewCandidateList);
+    }
+
+
+    filterUserList(filters: any, candidateList: any): void {
+
+      this.filteredUsers = candidateList; //Reset User List
+      const keys = Object.keys(filters);
+
+      const filterUser = user => {
+        let result = keys.map(key => {
+          if (key == "employeeName" || key == "JRSS") {
+            if (user.result_users[0][key]) {
+              return String(user.result_users[0][key]).toLowerCase().startsWith(String(filters[key]).toLowerCase())
+            }
+          }
+
+          else if (user[key]) {
+            return String(user[key]).toLowerCase().startsWith(String(filters[key]).toLowerCase())
+          } else {
+            return false;
+          }
+        });
+
+        // To Clean Array from undefined if the age is passed so the map will fill the gap with (undefined)
+        result = result.filter(it => it !== undefined);
+        return result.reduce((acc, cur: any) => { return acc & cur }, 1)
+      }
+      this.filteredUsers = candidateList.filter(filterUser);
+      this.TechnicalInterviewList = this.filteredUsers
+
+    }
+
+
 }
