87074a90b21d4822093b7b8d8abe526ab70d3622
Ranjeet Kumar <ranjeku5@in.ibm.com>
2020-08-13 13:47:28 +0530

    Updated for ops account search functionality

--- src/app/components/operations-candidate-search/operations-candidate-search.component.ts
+++ src/app/components/operations-candidate-search/operations-candidate-search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ApiService } from './../../service/api.service';
 
 @Component({
   selector: 'app-operations-candidate-search',
@@ -16,10 +17,14 @@ import { Router } from '@angular/router';
 export class OperationsCandidateSearchComponent implements OnInit {
   form: FormGroup;
   account: String = "";
+  AccountData:any = [];
+  AccountList:any=[];
+
   @Output() groupFilters: EventEmitter<any> = new EventEmitter<any>();
   searchText: string = '';
-  constructor(private fb: FormBuilder, private router: Router) {
+  constructor(private apiService: ApiService, private fb: FormBuilder, private router: Router) {
     this.account = this.router.getCurrentNavigation().extras.state.account;
+    this.readAccount();
   }
   ngOnInit(): void {
     this.buildForm();
@@ -30,6 +35,26 @@ export class OperationsCandidateSearchComponent implements OnInit {
       account: new FormControl('')
     });
   }
+
+    // Get all Accounts
+    readAccount(){
+      this.apiService.getAccounts().subscribe((data) => {
+      this.AccountData = data;
+      //Remove 'sector' from Account collection
+      for (var accValue of this.AccountData){    
+          if(accValue.account.toLowerCase() !== 'sector' ) {
+            this.AccountList.push(accValue.account);             
+          }            
+      }      
+      })
+    }
+  
+      // Choose account result with select dropdown
+      updateAccountDetails(e) {
+        this.form.get('account').setValue(e, {
+        onlySelf: true
+        })
+      }
 
   search(filters: any): void {
     Object.keys(filters).forEach(key => (filters[key] === '' || filters[key] ===  null) ? delete filters[key] : key);
