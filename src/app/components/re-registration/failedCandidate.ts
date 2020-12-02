fdfdf8a00426dc4754cde2e6b818c5e2fb696f85
Harshitha Edpuganti <edharshi@in.ibm.com>
2020-12-02 14:02:23 +0530

    #316- failed candidates

--- src/app/components/re-registration/failedCandidate.ts
+++ src/app/components/re-registration/failedCandidate.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@angular/core';
-import { CommandName } from 'protractor';
-import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
 import { ApiService } from '../../service/api.service';
 import { RegistrationConfigService } from '../../service/registrationConfig.service';
 
@@ -11,31 +9,23 @@ import { RegistrationConfigService } from '../../service/registrationConfig.serv
 export class failedCandidate {
   candidatesRetainDay : any;
   constructor(private registrationConfigService: RegistrationConfigService,private apiService : ApiService) {
-    this.getCandidatesRegistrationRetainDays();
   }
 
 // Candidate who has failed quiz should be made available or registered by other account after 'x'(7 days) no of days
 
-  // Get candidate registration retain days
-  getCandidatesRegistrationRetainDays(){
-    this.registrationConfigService.getStageCandidatesRetainDay().subscribe((data) => {
-      this.candidatesRetainDay = data;
-      console.log("RetainFailedCanidates days =" + this.candidatesRetainDay[0].RetainFailedCandidates);
-    })
-  }
- 
-
   isCandidateFailed(username) {
-    this.getCandidatesRegistrationRetainDays();
+      let noOfDays : number;
       let currentDate: Date = new Date();
       let userResult : string = "Fail";
       var resultCreatedDate: Date;
       let isCandidateReleased : boolean = false;
+      this.registrationConfigService.getStageCandidatesRetainDay().subscribe((resp) => {
+      noOfDays = resp[0].retainFailedCandidates;    
       this.apiService.getUserByUserName(username).subscribe(
         (res) => {
           this.apiService.getResultByUserResultFail(username,res.quizNumber,userResult).subscribe(data => {
             resultCreatedDate = new Date(data['createdDate']);
-            resultCreatedDate.setDate(resultCreatedDate.getDate() + this.candidatesRetainDay[0].RetainFailedCandidates);
+            resultCreatedDate.setDate(resultCreatedDate.getDate() + noOfDays);
             if (resultCreatedDate >= currentDate) {
               isCandidateReleased =  false;
             } else {
@@ -50,5 +40,6 @@ export class failedCandidate {
           console.log("Error found while fetching records from Users collection - " + error);
       });     
       return isCandidateReleased;
+    });
   } 
 }
