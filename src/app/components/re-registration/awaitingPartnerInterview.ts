2079a3713f4161b36b4dd5b36128a2447202f223
Ranjeet Kumar <ranjeku5@in.ibm.com>
2020-12-03 09:36:50 +0530

    Updated for Partner Awaiting Candidate

--- src/app/components/re-registration/awaitingPartnerInterview.ts
+++ src/app/components/re-registration/awaitingPartnerInterview.ts
@@ -7,32 +7,41 @@ import { RegistrationConfigService } from '../../service/registrationConfig.serv
 })
 
 export class AwaitingPartnerInterview {
-  constructor(private registrationConfigService: RegistrationConfigService,private apiService : ApiService){ }
+  candidatesRetainDay: Object;
 
-  // Candidate awaiting partner interview should be made available or registered by other account after n number of days
-  isCandidateAwaitingInPartnerInterviewQ(username,quizNumber) { 
-    var resultCreatedDate: Date;
-    let isCandidateReleased : boolean = false;
+    constructor(private registrationConfigService: RegistrationConfigService,private apiService : ApiService){ 
+      this.getCandidatesRegistrationRetainDays();
+    }
 
-    // Get retainStage4Candidates column value from RegistrationConfig table
-    this.registrationConfigService.getStageCandidatesRetainDay().subscribe((res) => {      
-        // Get candidate awaiting partner interview result
-        this.apiService.getPartnerCandidateAwaitingResult(username,quizNumber).subscribe((data) => {
-            resultCreatedDate = new Date(data['smeAssessmentDate']);
-            resultCreatedDate.setDate(resultCreatedDate.getDate() + res[0].retainStage4Candidates);
-            let currentDate: Date = new Date();
-            if (resultCreatedDate >= currentDate) {
+    // Get candidate stage's retain day from RegistrationConfig table
+    getCandidatesRegistrationRetainDays(){
+      this.registrationConfigService.getStageCandidatesRetainDay().subscribe((data) => {
+        this.candidatesRetainDay = data;      
+      })
+    }
+
+    // Candidate awaiting partner interview should be made available or registered by other account after n number of days
+    isCandidateAwaitingInPartnerInterviewQ(username,quizNumber) { 
+      var resultCreatedDate: Date;
+      let isCandidateReleased : boolean = false;
+    
+      // Get candidates partner awaiting interview result
+      this.apiService.getPartnerCandidateAwaitingResult(username,quizNumber).subscribe((data) => {
+          resultCreatedDate = new Date(data['smeAssessmentDate']);
+          resultCreatedDate.setDate(resultCreatedDate.getDate() + this.candidatesRetainDay[0].retainStage4Candidates);
+          let currentDate: Date = new Date();
+          if (resultCreatedDate >= currentDate) {
               isCandidateReleased =  false;
-            } else {
+          } else {
               isCandidateReleased = true;
-            }
+          }
+          //return isCandidateReleased;
         }, (error) => {
           console.log('[AwaitingPartnerInterview]-Error found while fetching the records for partner awaiting candidate');
           console.log(error);
           //return isCandidateReleased;
-        });
-        //return isCandidateReleased;
-    });
-    return isCandidateReleased;
-  }
+          });        
+        return isCandidateReleased;
+    }
+
 }
